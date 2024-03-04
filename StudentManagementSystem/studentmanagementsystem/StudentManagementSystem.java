651a40aa6cd5406e6da92733b2f1d2f981e22ad2
Simphiwe-Sithebe <108188116+Simphiwe-Sithebe@users.noreply.github.com>
2024-03-04 21:07:08 +0200

    Create StudentManagementSystem.java

---
+++ StudentManagementSystem/studentmanagementsystem/StudentManagementSystem.java
@@ -0,0 +1,20 @@
+package studentmanagementsystem;
+
+import studentmanagementsystem.ui.StudentManagementSystemGUI;
+
+/**
+ *
+ * @author Simphiwe-2021
+ */
+public class StudentManagementSystem {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        StudentManagementSystemGUI gui = new StudentManagementSystemGUI();
+        gui.setVisible(true);
+    }
+    
+}
